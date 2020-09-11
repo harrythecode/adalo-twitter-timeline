ed9697a6c582b659884c0cd80102d809f7b014bf
amezousan <goesbyamezou@gmail.com>
2020-09-11 06:34:00 +0000

    #1 Fixes for iOS/Android

--- src/components/TwitterTimeline/index.js
+++ src/components/TwitterTimeline/index.js
@@ -1,70 +1,51 @@
-/***
- * Copyright (c) 2020 Andrew Suzuki
- * Released under the MIT license
- * https://github.com/andrewsuzuki/react-twitter-widgets
- */
-import { Timeline } from 'react-twitter-widgets'
-import { Component } from 'react'
+import React, { Component } from 'react'
+import { WebView } from 'react-native-webview'
+import PropTypes from 'prop-types';
 
 class TwitterTimeline extends Component {
-	constructor(props) {
-		super(props);
-		this.state = { error: null, errorInfo: null };
-	}
 
-	getTimelineOptions(options) {
-		let chromeLists = [];
-		if (options.noheader) chromeLists.push("noheader");
-		if (options.nofooter) chromeLists.push("nofooter");
-		if (options.noborders) chromeLists.push("noborders");
-		if (options.transparent) chromeLists.push("transparent");
-		if (options.noscrollbar) chromeLists.push("noscrollbar");
+    static get propTypes() { 
+        return {
+            screenName: PropTypes.string,
+            timelineStyle: PropTypes.object,
+            timelineOptions: PropTypes.object
+        }; 
+    }
 
-		return chromeLists.join(" ");
-	}
-	
-	componentDidCatch(error, errorInfo) {
-		this.setState({
-			error: error,
-			errorInfo: errorInfo
-		})
-	}
+    getTimelineOptions(options) {
+        let chromeLists = [];
+        if (options.noheader) chromeLists.push("noheader");
+        if (options.nofooter) chromeLists.push("nofooter");
+        if (options.noborders) chromeLists.push("noborders");
+        if (options.transparent) chromeLists.push("transparent");
+        if (options.noscrollbar) chromeLists.push("noscrollbar");
 
-	render() {
-		if (this.state.errorInfo) {
-			return (
-			  <div>
-				<h2>Something went wrong.</h2>
-				{this.state.error && this.state.error.toString()}
-				<details style={{ whiteSpace: 'pre-wrap' }}>
-				  {this.state.error && this.state.error.toString()}
-				  <br />
-				  {this.state.errorInfo.componentStack}
-				</details>
-			  </div>
-			);
-		}
-		
-		const { screenName, timelineStyle, timelineOptions } = this.props;
-		const chromeLists = this.getTimelineOptions(timelineOptions);
-		
-		return (
-			<Timeline
-			  dataSource={{
-				sourceType: 'profile',
-				screenName: screenName
-			  }}
-			  options={{
-				chrome     : chromeLists,
-				height     : timelineStyle.height == 0 ? null : timelineStyle.height,
-				lang       : timelineOptions.lang,
-				theme      : timelineStyle.theme,
-				tweetLimit : timelineStyle.tweetLimit == 0 ? null : timelineStyle.tweetLimit,
-			  }}
-			renderError={(_err) => <p>Could not load timeline due to Error on Twitter side (Reason: {_err})</p>}
-			/>
-		)
-	}
+        return chromeLists.join(" ");
+    }
+
+    render() {
+        const { screenName, timelineStyle, timelineOptions } = this.props;
+        const chromeLists    = this.getTimelineOptions(timelineOptions);
+        const timelineHeight = timelineStyle.height == 0 ? null : timelineStyle.height;
+        const timelineLimit  = timelineStyle.tweetLimit == 0 ? null : timelineStyle.tweetLimit;
+        const htmlContent    = `
+        <a class="twitter-timeline"
+            href="https://twitter.com/${screenName}?ref_src=twsrc%5Etfw"
+            data-chrome="${chromeLists}"
+            data-height="${timelineHeight}"
+            data-lang="${timelineOptions.lang}"
+            data-theme="${timelineStyle.theme}"
+            data-tweetLimit="${timelineLimit}"
+        >Tweets by ${screenName}</a>
+        <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+        `;
+        console.log("render")
+        return (
+            <WebView
+                source={{html: htmlContent}}
+            />
+        )
+    }
 }
 
 export default TwitterTimeline
