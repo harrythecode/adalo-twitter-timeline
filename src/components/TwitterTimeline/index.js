fb4f4e8ad6c0df3be21f8e87ce6876981f1ad053
amezousan <goesbyamezou@gmail.com>
2020-09-15 09:44:48 +0200

    Fix/adalo review (#6)
    * Improve error handlings, help text, and so on from #5

--- src/components/TwitterTimeline/index.js
+++ src/components/TwitterTimeline/index.js
@@ -1,15 +1,35 @@
+/***
+ * Copyright (c) 2020 Andrew Suzuki
+ * Released under the MIT license
+ * https://github.com/andrewsuzuki/react-twitter-widgets
+ */
+import { Timeline } from 'react-twitter-widgets'
 import React, { Component } from 'react'
+import { StyleSheet, Platform, Text, View, Image } from 'react-native'
 import { WebView } from 'react-native-webview'
-import PropTypes from 'prop-types';
+import logo from './preview-thumbnail.png';
 
 class TwitterTimeline extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            hasError           : false,
+            errorType          : '',
+            width              : null,
+            height             : null,
+            previousScreenName : '',
+        };
+    }
 
-    static get propTypes() { 
-        return {
-            screenName: PropTypes.string,
-            timelineStyle: PropTypes.object,
-            timelineOptions: PropTypes.object
-        }; 
+    static getDerivedStateFromError(error) {
+        const regex   = new RegExp('ensure%20the%20screenName%2FtweetId%20exists');
+        let errorType = '';
+        if(regex.exec(error)){
+            errorType = "screenNameNotFound"
+        } else {
+            errorType = "unknown"
+        } 
+        return { hasError: true , errorType };
     }
 
     getTimelineOptions(options) {
@@ -23,26 +43,127 @@ class TwitterTimeline extends Component {
         return chromeLists.join(" ");
     }
 
+    handleLayout = ({ nativeEvent }) => {
+        const { editor } = this.props
+    
+        if (editor) return
+    
+        const { width, height } = (nativeEvent && nativeEvent.layout) || {}
+        const { width: prevWidth, height: prevHeight } = this.state
+    
+        if (width !== prevWidth || height !== prevHeight) {
+          this.setState({ width, height })
+        }
+    }
+
     render() {
-        const { screenName, timelineStyle, timelineOptions } = this.props;
+        const { screenName, timelineOptions, _height, editor } = this.props;
         const chromeLists    = this.getTimelineOptions(timelineOptions);
-        const timelineHeight = timelineStyle.height == 0 ? null : timelineStyle.height;
-        const timelineLimit  = timelineStyle.tweetLimit == 0 ? null : timelineStyle.tweetLimit;
+        const timelineHeight = timelineOptions.nomaxheight ? _height : null;
+        const timelineLimit  = timelineOptions.tweetLimit == 0 ? null : timelineOptions.tweetLimit;
         const htmlContent    = `
+        <div id="twitter-container-for-webview-20200915" style="height: 100%; width: 100%;">
         <a class="twitter-timeline"
             href="https://twitter.com/${screenName}?ref_src=twsrc%5Etfw"
             data-chrome="${chromeLists}"
             data-height="${timelineHeight}"
             data-lang="${timelineOptions.lang}"
-            data-theme="${timelineStyle.theme}"
+            data-theme="${timelineOptions.theme}"
             data-tweetLimit="${timelineLimit}"
         >Tweets by ${screenName}</a>
+        </div>
         <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
         `;
-        console.log("render")
+        
+        const styles = StyleSheet.create({
+            twContainer: {
+                justifyContent: "center",
+                alignItems: "center",
+                backgroundColor: "#333333",
+                color: "#eeeeee",
+                fontWeight: "bold",
+                padding: 10,
+                width: "100%",
+                height: "100%"
+            },
+            titleBody: {
+                fontSize: 14
+            },
+            customMargin: {
+                marginBottom : timelineOptions.nomaxheight ? 0 : timelineOptions.tweetMarginBottom
+            }
+        });
+
+        // If it's editor, don't trigger twitter widget
+        if(editor) {
+            return(
+            <View style={styles.twContainer} onLayout={this.nativeEvent}>
+                <img src={logo} style={{width: "100%"}} />
+            </View>
+            )
+        }
+
+        // Check if timeline has any error
+        if (this.state.hasError) {
+            let errorMessage = 'Something went wrong';
+            if (this.state.errorType == "screenNameNotFound") {
+                errorMessage = screenName + ' does not exist'
+            }
+            if(this.state.previousScreenName !== screenName) {
+                this.state = {
+                    hasError: false,
+                    previousScreenName: screenName
+                }
+            }
+            return (
+                <View style={styles.twContainer}>
+                    <Text style={styles.titleBody}>Error: {errorMessage}</Text>
+                </View>
+            )
+        }
+
+        // Workaround:
+        // - When widget.js gets an error, it will add "twitter-timeline-error" class name to the a tag
+        //   so I just set a trigger if it has a specific class name or not.
+        // - Added setTimeout since it takes a few mili-seconds to render DOM
+        const INJECTED_JAVASCRIPT = `setTimeout(function() {
+            var content    = document.getElementById("twitter-container-for-webview-20200915");
+            var contentFlg = content.getElementsByClassName("twitter-timeline-error");
+
+            if(contentFlg.length > 0) {
+                contentFlg[0].outerHTML="<h1 style='font-size:72; padding:50;'>Error: ${screenName} does not exist</h1>"
+                content.style.backgroundColor="#333333"
+                content.style.justifyContent="center"
+                content.style.alignItems="center"
+                content.style.display="flex"
+                content.style.color="#eeeeee"
+            }
+        }, 50);`;
+
         return (
+            Platform.OS === 'web'
+            ?  <View style={styles.customMargin} onLayout={this.handleLayout}>
+                    <Timeline
+                    dataSource={{
+                        sourceType: 'profile',
+                        screenName: screenName
+                    }}
+                    options={{
+                        chrome     : chromeLists,
+                        height     : timelineHeight,
+                        lang       : timelineOptions.lang,
+                        theme      : timelineOptions.theme,
+                        tweetLimit : timelineOptions.tweetLimit == 0 ? null : timelineOptions.tweetLimit,
+                    }}
+                    renderError={(_err) => <p>Could not load timeline due to Error on Twitter side (Reason: {_err})</p>}
+                    />
+                </View>
+            : 
             <WebView
+                originWhitelist={['*']}
+                style={styles.customMargin}
                 source={{html: htmlContent}}
+                injectedJavaScript={INJECTED_JAVASCRIPT}
             />
         )
     }
